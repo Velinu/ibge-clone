0bafd2579716a81f1cd1bbb310e10301d18952dd
Velinu <matheusfelintosilva2@gmail.com>
2024-06-09 23:17:48 -0300

    a

--- js/header.js
+++ js/header.js
@@ -2,17 +2,18 @@ const filterBtn = document.querySelector("#filter-btn");
 const filterDialog = document.querySelector("#filter-dialog");
 const searchForm = document.querySelector("#search-form");
 const filterForm = document.querySelector("#form-dialog");
+const searchInput = document.querySelector("#search-input");
 const newslist = document.querySelector("#news-list");
 const searchParams = new URLSearchParams(window.location.search);
 const closeModalBtn = document.querySelector('#close-modal-btn')
 
 closeModalBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    filterDialog.close()
+  filterDialog.close();
+  e.preventDefault();
 })
 
 filterBtn.addEventListener("click", () => {
-  filterDialog.showModal();
+  filterDialog.showModal(); 
 });
 
 async function searchNews(params) {
@@ -26,36 +27,6 @@ async function getJsonData(url) {
   const data = await res.json();
   return data;
 }
-
-document.addEventListener("DOMContentLoaded", async (event) => {
-    const data = await searchNews(window.location.search);
-    const main = document.querySelector("main");
-    const ul = document.createElement("ul");
-    main.appendChild(ul);
-    data.items.forEach(async (e) => {
-      const dias = diferencaData(e.data_publicacao);
-      const img = await gerarJsonImagens(e.imagens)
-      const li = document.createElement("li");
-      li.innerHTML = `
-          <div class="news-card">
-          <img src=${img} />
-          <div class="news-detail-container">
-              <div id="name-descr-container">
-                <h2>${e.titulo}</h2>
-                <p>${e.introducao}</p>
-              </div>
-              <div class="date-type-conainer">
-                <p id="category">${e.tipo}</p>
-                <p id="date">Publicado ${
-                  dias === 0 ? "hoje" : dias === 1 ? "há 1 dia" : `há ${dias} dias`
-                }</p>
-              </div>
-            <a href=${e.link} target=_blank>Leia mais</a>
-          </div>
-          </div>`;
-      ul.appendChild(li);
-    });
-  });
 
 const diferencaData = (data) => {
   const dataAtual = new Date();
--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
 
+
+//window.location.search = "busca=&tipo=release&qtd=5&de=&ate=";
+document.addEventListener("DOMContentLoaded", async () => {
+    filterDialog.close();
+
+    if (!window.location.search) {
+      window.location.search = "busca=&tipo=release&qtd=5&de=&ate=&page=1";
+  }
+    
+    const data = await searchNews(window.location.search);
+    console.log(window.location.search)
+    addPagination(data, 5, window.location.search);
+    const main = document.querySelector("main");
+    const ul = document.createElement("ul");
+    main.appendChild(ul);
+    data.items.forEach(async (e) => {
+      const dias = diferencaData(e.data_publicacao);
+      const img = await gerarJsonImagens(e.imagens)
+      const li = document.createElement("li");
+      li.innerHTML = `
+          <div class="news-card">
+          <img src=${img} />
+          <div class="news-detail-container">
+              <div id="name-descr-container">
+                <h2>${e.titulo}</h2>
+                <p>${e.introducao}</p>
+              </div>
+              <div class="date-type-conainer">
+                <p id="category">#${e.editorias}</p>
+                <p id="date">Publicado ${
+                  dias === 0 ? "hoje" : dias === 1 ? "há 1 dia" : `há ${dias} dias`
+                }</p>
+              </div>
+            <a href=${e.link} target=_blank>Leia mais</a>
+          </div>
+          </div>`;
+      ul.appendChild(li);
+    });
+  });
+
+  const addPagination = (data, currentPage, params) => {
+    const urlParams = new URLSearchParams(params);
+    const pagination = document.querySelector("#pagination");
+    const totalPages = Math.ceil(data.totalPages / 10);
+    const startPage = Math.max(1, currentPage - 4);
+    const endPage = Math.min(totalPages, currentPage + 10);
+  
+    for (let i = startPage; i <= endPage; i++) {
+      const li = document.createElement("li");
+      const button = document.createElement("button");
+      button.textContent = i;
+      button.style.backgroundColor = i === currentPage ? "#4682b4" : "initial";
+      button.onclick = () => {
+        urlParams.set("page", i);
+        window.location.search = urlParams.toString();
+      };
+      
+      li.appendChild(button);
+      pagination.appendChild(li);
+    }
+  }
